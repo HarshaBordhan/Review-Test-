28c251e98d21c8865bece5dcb6208ca3bcdf7322
Harsha Bordhan Chakma <harshabordhanc@gmail.com>
2023-05-07 20:04:12 +0600

    Testing Carousel

--- components/Figma/main.js
+++ components/Figma/main.js
@@ -2,7 +2,7 @@ import Link from "next/link";
 
 export default function Image({ hoverText, children, className }) {
   return (
-    <div className={`pr-[24px] relative ${className}`}>
+    <div className={`pr-[36px] relative ${className}`}>
       <Link href="#">
         <div className="hover-text">{hoverText}</div>
         <div className="">{children}</div>
