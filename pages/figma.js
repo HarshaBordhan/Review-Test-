3daed0725097a7e71975ed37becefbe1d08067ab
Harsha Bordhan Chakma <harshabordhanc@gmail.com>
2023-05-07 14:37:26 +0600

    add draggable feature

--- pages/figma.js
+++ pages/figma.js
@@ -3,15 +3,6 @@ import Link from "next/link";
 import Main from "../components/Figma/main";
 
 import React, { useEffect, useRef, useState } from "react";
-/*
-import { Swiper, SwiperSlide } from "swiper/react";
-// import swiper styles
-import "swiper/css";
-import "swiper/css/navigation";
-import "swiper/css/free-mode";
-
-import { Navigation, FreeMode } from "swiper";
-*/
 
 import Photos from "../components/Figma/photos";
 
@@ -141,26 +132,8 @@ export default function Figma() {
       <main className="">
         <section className="w-full">
           <div className="bg-[#ffc700] border-b-4 border-black relative pt-[82px] pb-[20px]">
-            {/* <div className=""> */}
             <div className="overflow-hidden relative w-full slide-container">
               <div className="flex w-full mx-auto my-0 overflow-x-hidden scroll-smooth carousel">
-                {/* <div className="w-full mx-auto my-0 carousel"> */}
-                {/* <div className=""> */}
-                {/* <Swiper
-                  slidesPerView={"auto"}
-                  loop={true}
-                  freeMode={true}
-                  spaceBetween={0}
-                  centeredSlides={true}
-                  autoHeight={true}
-                  navigation={{
-                    nextEl: ".button-next-slide",
-                    prevEl: ".button-prev-slide",
-                  }}
-                  modules={[Navigation, FreeMode]}
-                  className="mySwiper flex justify-between"
-                > */}
-                {/* <SwiperSlide> */}
                 <Main
                   hoverText="Version history coach jacket"
                   className="slide"
@@ -198,9 +171,7 @@ export default function Figma() {
                     />
                   </svg>
                 </Main>
-                {/* </SwiperSlide> */}
 
-                {/* <SwiperSlide> */}
                 <Main hoverText="Gridlock washi tape" className="slide">
                   <svg
                     className="svg-cutout"
@@ -237,9 +208,7 @@ export default function Figma() {
                     />
                   </svg>
                 </Main>
-                {/* </SwiperSlide> */}
 
-                {/* <SwiperSlide> */}
                 <Main hoverText="Tidy up tee" className="slide">
                   <svg
                     className="svg-cutout"
@@ -274,9 +243,7 @@ export default function Figma() {
                     />
                   </svg>
                 </Main>
-                {/* </SwiperSlide> */}
 
-                {/* <SwiperSlide> */}
                 <Main hoverText="Design in bloom tee" className="slide">
                   <svg
                     className="svg-cutout"
@@ -311,9 +278,7 @@ export default function Figma() {
                     />
                   </svg>
                 </Main>
-                {/* </SwiperSlide> */}
 
-                {/* <SwiperSlide> */}
                 <Main hoverText="Dress code hat" className="slide">
                   <svg
                     className="svg-cutout"
@@ -348,9 +313,7 @@ export default function Figma() {
                     />
                   </svg>
                 </Main>
-                {/* </SwiperSlide> */}
 
-                {/* <SwiperSlide> */}
                 <Main hoverText="Shape up tee" className="slide">
                   <svg
                     className="svg-cutout"
@@ -387,9 +350,7 @@ export default function Figma() {
                     />
                   </svg>
                 </Main>
-                {/* </SwiperSlide> */}
 
-                {/* <SwiperSlide> */}
                 <Main hoverText="Toolkit pin" className="slide">
                   <svg
                     className="svg-cutout"
@@ -428,9 +389,7 @@ export default function Figma() {
                     />
                   </svg>
                 </Main>
-                {/* </SwiperSlide> */}
 
-                {/* <SwiperSlide> */}
                 <Main hoverText="Band together socks" className="slide">
                   <svg
                     className="svg-cutout"
@@ -465,9 +424,7 @@ export default function Figma() {
                     />
                   </svg>
                 </Main>
-                {/* </SwiperSlide> */}
 
-                {/* <SwiperSlide> */}
                 <Main hoverText="Framework tee" className="slide">
                   <svg
                     className="svg-cutout"
@@ -502,9 +459,7 @@ export default function Figma() {
                     />
                   </svg>
                 </Main>
-                {/* </SwiperSlide> */}
 
-                {/* <SwiperSlide> */}
                 <Main hoverText="Design in bloom tee" className="slide">
                   <svg
                     className="svg-cutout"
@@ -541,8 +496,169 @@ export default function Figma() {
                     />
                   </svg>
                 </Main>
-                {/* </SwiperSlide> */}
-                {/* </Swiper> */}
+
+                {/* <div className={`pr-[24px] slide relative`}>
+                  <Link href="#">
+                    <div className="hover-text">
+                      Version history coach jacket
+                    </div>
+                    <div className="">
+                      <svg
+                        className="svg-cutout"
+                        width={1124}
+                        height={612}
+                        viewBox="0 0 1124 612"
+                        fill="none"
+                        xmlns="http://www.w3.org/2000/svg"
+                        xmlnsXlink="http://www.w3.org/1999/xlink"
+                      >
+                        <defs>
+                          <pattern
+                            id="188d1671-d29a-4af9-815d-2e775ba8a897"
+                            patternUnits="objectBoundingBox"
+                            width={1}
+                            height={1}
+                          >
+                            <image
+                              href="//cdn.shopify.com/s/files/1/0576/8364/0503/files/figma-store_homepage-hero_01_1000x.jpg?v=1678222214"
+                              x={0}
+                              y={0}
+                              width="100%"
+                              height="100%"
+                              className="pattern-image"
+                              preserveAspectRatio="xMidYMid slice"
+                            />
+                          </pattern>
+                        </defs>
+                        <path
+                          d="M302.279 498.312L300 493.256L297.721 498.312C266.868 566.765 220.337 609.5 169 609.5C123.66 609.5 82.0782 576.193 51.6874 521.166C21.3451 466.227 2.5 390.159 2.5 306C2.5 221.841 21.3451 145.773 51.6874 90.834C82.0782 35.8068 123.66 2.5 169 2.5C220.337 2.5 266.868 45.2351 297.721 113.688L300 118.744L302.279 113.688C333.132 45.2351 379.663 2.5 431 2.5C482.337 2.5 528.868 45.2351 559.721 113.688L562 118.744L564.279 113.688C595.132 45.2351 641.663 2.5 693 2.5C744.337 2.5 790.868 45.2351 821.721 113.688L824 118.744L826.279 113.688C857.132 45.2351 903.663 2.5 955 2.5C1000.34 2.5 1041.92 35.8068 1072.31 90.834C1102.65 145.773 1121.5 221.841 1121.5 306C1121.5 390.159 1102.65 466.227 1072.31 521.166C1041.92 576.193 1000.34 609.5 955 609.5C903.663 609.5 857.132 566.765 826.279 498.312L824 493.256L821.721 498.312C790.868 566.765 744.337 609.5 693 609.5C641.663 609.5 595.132 566.765 564.279 498.312L562 493.256L559.721 498.312C528.868 566.765 482.337 609.5 431 609.5C379.663 609.5 333.132 566.765 302.279 498.312Z"
+                          fill="url(#188d1671-d29a-4af9-815d-2e775ba8a897)"
+                        />
+                      </svg>
+                    </div>
+                  </Link>
+                </div>
+                <div className={`pr-[24px] slide relative`}>
+                  <Link href="#">
+                    <div className="hover-text">Gridlock washi tape</div>
+                    <div className="">
+                      {" "}
+                      <svg
+                        className="svg-cutout"
+                        width={658}
+                        height={658}
+                        viewBox="-2 -2 654 654"
+                        fill="none"
+                        xmlns="http://www.w3.org/2000/svg"
+                        xmlnsXlink="http://www.w3.org/1999/xlink"
+                      >
+                        <defs>
+                          <pattern
+                            id="a2fff064-d150-402f-950f-f53d970c7949"
+                            patternUnits="objectBoundingBox"
+                            width={1}
+                            height={1}
+                          >
+                            <image
+                              href="//cdn.shopify.com/s/files/1/0576/8364/0503/files/figma-store_homepage-hero_02_1000x.jpg?v=1678222214"
+                              x={0}
+                              y={0}
+                              width="100%"
+                              height="100%"
+                              className="pattern-image"
+                              preserveAspectRatio="xMidYMid slice"
+                            />
+                          </pattern>
+                        </defs>
+                        <path
+                          fillRule="evenodd"
+                          clipRule="evenodd"
+                          d="M650 0H0V125.496C0 231.179 81.9707 317.718 185.802 325C81.9707 332.282 0 418.821 0 524.504V650H650V524.504C650 418.82 568.029 332.282 464.198 325C568.029 317.718 650 231.179 650 125.496V0Z"
+                          fill="url(#a2fff064-d150-402f-950f-f53d970c7949)"
+                        />
+                      </svg>
+                    </div>
+                  </Link>
+                </div>
+                <div className={`pr-[24px] slide relative`}>
+                  <Link href="#">
+                    <div className="hover-text">Design in bloom tee</div>
+                    <div className="">
+                      <svg
+                        className="svg-cutout"
+                        width={650}
+                        height={650}
+                        viewBox="0 0 650 650"
+                        fill="none"
+                        xmlns="http://www.w3.org/2000/svg"
+                        xmlnsXlink="http://www.w3.org/1999/xlink"
+                      >
+                        <defs>
+                          <pattern
+                            id="4d1b2e4d-d66c-4b87-bbad-d753c62db021"
+                            patternUnits="objectBoundingBox"
+                            width={1}
+                            height={1}
+                          >
+                            <image
+                              href="//cdn.shopify.com/s/files/1/0576/8364/0503/files/figma-store_homepage-hero_03_1000x.jpg?v=1678222214"
+                              x={0}
+                              y={0}
+                              width="100%"
+                              height="100%"
+                              className="pattern-image"
+                              preserveAspectRatio="xMidYMid slice"
+                            />
+                          </pattern>
+                        </defs>
+                        <path
+                          d="M648 325C648 503.388 503.388 648 325 648C146.612 648 2 503.388 2 325C2 146.612 146.612 2 325 2C503.388 2 648 146.612 648 325Z"
+                          fill="url(#4d1b2e4d-d66c-4b87-bbad-d753c62db021)"
+                        />
+                      </svg>
+                    </div>
+                  </Link>
+                </div>
+                <div className={`pr-[24px] slide relative`}>
+                  <Link href="#">
+                    <div className="hover-text">Hwjoborolo</div>
+                    <div className="">
+                      {" "}
+                      <svg
+                        className="svg-cutout"
+                        width={1124}
+                        height={612}
+                        viewBox="0 0 1124 612"
+                        fill="none"
+                        xmlns="http://www.w3.org/2000/svg"
+                        xmlnsXlink="http://www.w3.org/1999/xlink"
+                      >
+                        <defs>
+                          <pattern
+                            id="9ec3ec12-3d86-4315-b660-2cbee1cedb72"
+                            patternUnits="objectBoundingBox"
+                            width={1}
+                            height={1}
+                          >
+                            <image
+                              href="//cdn.shopify.com/s/files/1/0576/8364/0503/files/figma-store_homepage-hero_04_1000x.jpg?v=1678222214"
+                              x={0}
+                              y={0}
+                              width="100%"
+                              height="100%"
+                              className="pattern-image"
+                              preserveAspectRatio="xMidYMid slice"
+                            />
+                          </pattern>
+                        </defs>
+                        <path
+                          d="M302.279 498.312L300 493.256L297.721 498.312C266.868 566.765 220.337 609.5 169 609.5C123.66 609.5 82.0782 576.193 51.6874 521.166C21.3451 466.227 2.5 390.159 2.5 306C2.5 221.841 21.3451 145.773 51.6874 90.834C82.0782 35.8068 123.66 2.5 169 2.5C220.337 2.5 266.868 45.2351 297.721 113.688L300 118.744L302.279 113.688C333.132 45.2351 379.663 2.5 431 2.5C482.337 2.5 528.868 45.2351 559.721 113.688L562 118.744L564.279 113.688C595.132 45.2351 641.663 2.5 693 2.5C744.337 2.5 790.868 45.2351 821.721 113.688L824 118.744L826.279 113.688C857.132 45.2351 903.663 2.5 955 2.5C1000.34 2.5 1041.92 35.8068 1072.31 90.834C1102.65 145.773 1121.5 221.841 1121.5 306C1121.5 390.159 1102.65 466.227 1072.31 521.166C1041.92 576.193 1000.34 609.5 955 609.5C903.663 609.5 857.132 566.765 826.279 498.312L824 493.256L821.721 498.312C790.868 566.765 744.337 609.5 693 609.5C641.663 609.5 595.132 566.765 564.279 498.312L562 493.256L559.721 498.312C528.868 566.765 482.337 609.5 431 609.5C379.663 609.5 333.132 566.765 302.279 498.312Z"
+                          fill="url(#9ec3ec12-3d86-4315-b660-2cbee1cedb72)"
+                        />
+                      </svg>
+                    </div>
+                  </Link>
+                </div> */}
               </div>
 
               <div className="buttons">
@@ -1546,3 +1662,33 @@ export default function Figma() {
 }
 */
 }
+
+/*
+import { Swiper, SwiperSlide } from "swiper/react";
+// import swiper styles
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/free-mode";
+
+import { Navigation, FreeMode } from "swiper";
+
+  <Swiper
+                  slidesPerView={"auto"}
+                  loop={true}
+                  freeMode={true}
+                  spaceBetween={0}
+                  centeredSlides={true}
+                  autoHeight={true}
+                  navigation={{
+                    nextEl: ".button-next-slide",
+                    prevEl: ".button-prev-slide",
+                  }}
+                  modules={[Navigation, FreeMode]}
+                  className="mySwiper flex justify-between"
+                >
+               <SwiperSlide></SwiperSlide> 
+               <SwiperSlide></SwiperSlide> 
+               <SwiperSlide></SwiperSlide> 
+               <SwiperSlide></SwiperSlide> 
+               <SwiperSlide></SwiperSlide> 
+*/
