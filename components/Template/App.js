e40e57d3263896310b6f520cb6774316aec8f7c0
Harsha Bordhan Chakma <harshabordhanc@gmail.com>
2023-05-12 15:32:44 +0600

    changed few things

--- components/Template/App.js
+++ components/Template/App.js
@@ -1,3 +1,4 @@
+/*
 import { motion } from "framer-motion";
 
 export default function App() {
@@ -6,6 +7,29 @@ export default function App() {
       className="box"
       initial={{ opacity: 0 }}
       animate={{ opacity: 1 }}
+    />
+  );
+}
+*/
+
+import { motion } from "framer-motion";
+
+export default function App() {
+  return (
+    <motion.div
+      className="box"
+      animate={{
+        scale: [1, 2, 2, 1, 1],
+        rotate: [0, 0, 180, 180, 0],
+        borderRadius: ["0%", "0%", "50%", "50%", "0%"],
+      }}
+      transition={{
+        duration: 2,
+        ease: "easeInOut",
+        times: [0, 0.2, 0.5, 0.8, 1],
+        repeat: Infinity,
+        repeatDelay: 1,
+      }}
     />
   );
 }
