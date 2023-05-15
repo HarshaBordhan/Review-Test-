95d4b00abc4a26545d6ec80b9dee5f751aaf61ef
Harsha Bordhan Chakma <harshabordhanc@gmail.com>
2023-05-15 20:34:49 +0600

    lenis

---
+++ pages/lenis.js
@@ -0,0 +1,13 @@
+export default function Lenis() {
+  return (
+    <>
+      <div>Auto Scroll</div>
+      <div>
+        <h2>Smoth Scroll</h2>
+        <div>This is lenis scroll</div>
+        <div>This is lenis scroll</div>
+        <div>This is lenis scroll</div>
+      </div>
+    </>
+  );
+}
