5b5e545aea93aa4c751a6adc05f9b4d44997b6d6
Harsha Bordhan Chakma <harshabordhanc@gmail.com>
2023-05-29 19:47:42 +0600

    lennis

--- pages/lenis.js
+++ pages/lenis.js
@@ -8,6 +8,10 @@ export default function Lenis() {
           This is lenis scroll. Lenis is a open source library built to
           standarize
         </p>
+        <p>
+          This is lenis scroll. Lenis is a open source library built to
+          standarize
+        </p>
       </div>
     </>
   );
