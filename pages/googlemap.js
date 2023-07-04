4180d218b2668c069cf020924b789df455e81656
Harsha Bordhan Chakma <harshabordhanc@gmail.com>
2023-07-04 11:02:47 +0600

    added map

--- pages/googlemap.js
+++ pages/googlemap.js
@@ -2,8 +2,9 @@ export default function Map() {
   return (
     <div>
       <h2>This page will generate a google map</h2>
-      <p>gonna add google map with google map api</p>
-      <span>quickly</span>
+      <p>
+        gonna add google map with google map api <span>quickly</span>
+      </p>
     </div>
   );
 }
