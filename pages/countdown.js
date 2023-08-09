7bd5c42add4b8000940d39049e258b814176b62d
Harsha Bordhan Chakma <harshabordhanc@gmail.com>
2023-08-09 22:33:41 +0600

    updated countdown

--- pages/countdown.js
+++ pages/countdown.js
@@ -11,7 +11,7 @@ export default function Countdown() {
     };
 
     const updateTime = function () {
-      const dest = new Date("August 29, 2023 11:59:00").getTime();
+      const dest = new Date("August 12, 2023 11:59:00").getTime();
       const now = new Date().getTime();
 
       const diff = dest - now;
